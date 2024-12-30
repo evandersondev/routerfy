9ddfdbad8ab55baee42c2a4097e10a831a223388
Evanderson Vasconcelos <evanderson.vasconcelos@pag.ai>
2024-12-30 16:40:57 -0300

    initial outlet config

--- lib/routerfy.dart
+++ lib/routerfy.dart
@@ -4,28 +4,32 @@ import 'package:url_strategy/url_strategy.dart';
 
 class Routerfy {
   static final Routerfy _instance = Routerfy._internal();
+
   Routerfy._internal() {
-    // Remova o "#" da URL no Flutter Web ao inicializar o Routerfy
     setPathUrlStrategy();
   }
 
   static Routerfy get instance => _instance;
+  static Routerfy get I => _instance;
 
-  final Map<String, Widget Function(BuildContext, RouterfyState)> _routeCache =
+  final Map<String, Widget Function(BuildContext, RouterfyState)> routeCache =
       {};
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   /// Configura as rotas e cacheia caminhos
   void configureRoutes(BrowserRouter routes) {
-    _routeCache.clear();
+    routeCache.clear();
     _flattenRoutes(routes.children, "");
-    print("Cached Routes: ${_routeCache.keys}");
   }
 
   void _flattenRoutes(List<RouterfyRoute> routes, String parentPath) {
     for (var route in routes) {
       final fullPath = "$parentPath${route.path}".replaceAll("//", "/");
-      _routeCache[fullPath] = route.builder;
+      routeCache[fullPath] = route.builder;
+
+      if (route.children.isNotEmpty) {
+        _flattenRoutes(route.children, fullPath);
+      }
     }
   }
 
@@ -34,7 +38,7 @@ class Routerfy {
     final path = uri.path;
 
     // Encontra rota correspondente
-    for (var route in _routeCache.entries) {
+    for (var route in routeCache.entries) {
       final parameters = <String>[];
       final regexp = pathToRegExp(route.key, parameters: parameters);
       final match = regexp.matchAsPrefix(path);
@@ -42,6 +46,7 @@ class Routerfy {
       if (match != null) {
         final params = extract(parameters, match);
         final state = RouterfyState(params, uri.queryParameters, settings);
+
         return PageRouteBuilder(
           pageBuilder: (context, animation, secondaryAnimation) =>
               route.value(context, state),
@@ -92,10 +97,12 @@ class BrowserRouter {
 class RouterfyRoute {
   final String path;
   final Widget Function(BuildContext, RouterfyState) builder;
+  final List<RouterfyRoute> children;
 
   RouterfyRoute({
     required this.path,
     required this.builder,
+    this.children = const [],
   });
 }
 
@@ -117,5 +124,53 @@ class NavLink extends StatelessWidget {
       },
       child: child,
     );
+  }
+}
+
+class Outlet extends StatelessWidget {
+  const Outlet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final settings = ModalRoute.of(context)?.settings;
+    final uri = Uri.parse(settings?.name ?? "/");
+    final path = uri.path;
+
+    final parameters = <String>[];
+
+    // print(path);
+    // print(Routerfy.I.routeCache[path]);
+
+    if (Routerfy.I.routeCache[path] != null) {
+      // final regexp = pathToRegExp(route.key, parameters: parameters);
+      // final match = regexp.matchAsPrefix(path);
+
+      return Text('DENTRO');
+    } else {
+      return const SizedBox.shrink();
+    }
+
+    // Encontra a rota correspondente
+    // for (var route in Routerfy.I.routeCache.entries) {
+
+    //   if (match != null) {
+    //   }
+
+    // if (match != null && route.value.children.isNotEmpty) {
+    // final subPath = path.replaceFirst(route.key, '').replaceAll('//', '/');
+
+    // for (var subRoute in route.value.children) {
+    //   final subRegexp = pathToRegExp(subRoute.path, parameters: parameters);
+    //   final subMatch = subRegexp.matchAsPrefix(subPath);
+
+    //   if (subMatch != null) {
+    //     final params = extract(parameters, subMatch);
+    //     final state = RouterfyState(params, uri.queryParameters, settings!);
+    //     return subRoute.builder(context, state);
+    //   }
+
+    // }
+    // }
+    // }
   }
 }
